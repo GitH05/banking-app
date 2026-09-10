d2e4da02851d5060b2e81729c4600acb39c1f0ec
GitH05 <santoshdhawal05@gmail.com>
2026-09-10 15:20:36 +0545

    bankdemoapp

--- src/main/java/com/backend/banking_app/controller/AccountController.java
+++ src/main/java/com/backend/banking_app/controller/AccountController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.backend.banking_app.dto.AccountDto;
 import com.backend.banking_app.service.AccountService;
 import org.springframework.web.bind.annotation.PostMapping;
