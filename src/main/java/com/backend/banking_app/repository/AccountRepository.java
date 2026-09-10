ede98c835c2645a832c1ab2ad22591136d92e76c
GitH05 <santoshdhawal05@gmail.com>
2026-09-10 15:05:28 +0545

    ref:

--- src/main/java/com/backend/banking_app/repository/AccountRepository.java
+++ src/main/java/com/backend/banking_app/repository/AccountRepository.java
@@ -2,7 +2,6 @@ package com.backend.banking_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.backend.banking_app.entity.Account;
 
 @Repository
