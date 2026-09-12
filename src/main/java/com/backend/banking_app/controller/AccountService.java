89aaf6003c6c5b080d5b7bddec476c8c3edf9ea7
GitH05 <santoshdhawal05@gmail.com>
2026-09-12 08:15:33 +0545

    ref: removed unwanted commentslimports

--- src/main/java/com/backend/banking_app/controller/AccountService.java
+++ src/main/java/com/backend/banking_app/controller/AccountService.java
@@ -3,6 +3,6 @@ package com.backend.banking_app.controller;
 /**
  * AccopuntService
  */
-public class AccopuntService {
+public class AccountService {
 
 }
--- src/main/java/com/backend/banking_app/service/AccountService.java
+++ src/main/java/com/backend/banking_app/service/AccountService.java
@@ -1,7 +1,6 @@
 package com.backend.banking_app.service;
 
 import com.backend.banking_app.dto.AccountDto;
-import com.backend.banking_app.entity.Account;
 
 public interface AccountService {
     AccountDto createAccount(AccountDto accountDto);
