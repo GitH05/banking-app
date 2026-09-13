cd6db6360d3cb76a0c7c44a65da05f2cb165a96b
GitH05 <santoshdhawal05@gmail.com>
2026-09-13 12:43:52 +0545

    ref: document formatted

--- src/main/java/com/backend/banking_app/controller/AccountController.java
+++ src/main/java/com/backend/banking_app/controller/AccountController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
@@ -63,6 +62,5 @@ public class AccountController {
         List<AccountDto> accounts = accountService.getAllAccounts();
         return ResponseEntity.ok(accounts);
     }
-    
 
 }
--- src/main/java/com/backend/banking_app/service/AccountService.java
+++ src/main/java/com/backend/banking_app/service/AccountService.java
@@ -6,8 +6,12 @@ import com.backend.banking_app.dto.AccountDto;
 
 public interface AccountService {
     AccountDto createAccount(AccountDto accountDto);
+
     AccountDto getAccountById(Long accountId);
+
     AccountDto deposit(Long accountId, Double balance);
+
     AccountDto withdraw(Long accountId, Double balance);
+
     List<AccountDto> getAllAccounts(); // Optional method to retrieve all accounts
 }
