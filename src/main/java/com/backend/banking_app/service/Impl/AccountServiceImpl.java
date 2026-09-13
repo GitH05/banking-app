bb9dd4ee4b9681408a98d48bf7ebd87ee661bd4b
GitH05 <santoshdhawal05@gmail.com>
2026-09-13 16:30:22 +0545

    feat: exception handled | formatted | implemented all basic functionality

--- src/main/java/com/backend/banking_app/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/backend/banking_app/service/Impl/AccountServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.backend.banking_app.dto.AccountDto;
 import com.backend.banking_app.entity.Account;
+import com.backend.banking_app.exception.AccountNotFoundException;
+import com.backend.banking_app.exception.InSufficientBalanceException;
 import com.backend.banking_app.mapper.AccountMapper;
 import com.backend.banking_app.repository.AccountRepository;
 import com.backend.banking_app.service.AccountService;
@@ -23,54 +25,67 @@ public class AccountServiceImpl implements AccountService {
     public AccountDto createAccount(AccountDto accountDto) {
         Account account = AccountMapper.mapAccountDtoToAccount(accountDto);
         Account savedAccount = accountRepository.save(account);
+
         return AccountMapper.mapAccountToAccountDto(savedAccount);
     }
 
     @Override
     public AccountDto getAccountById(Long accountId) {
         Account account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
+                .orElseThrow(() -> new AccountNotFoundException(
+                        "Account not found with id: " + accountId));
+
         return AccountMapper.mapAccountToAccountDto(account);
     }
 
     @Override
     public AccountDto deposit(Long accountId, Double balance) {
         Account account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
+                .orElseThrow(() -> new AccountNotFoundException(
+                        "Account not found with id: " + accountId));
+        ;
 
         double total = account.getBalance() + balance;
         account.setBalance(total);
+
         Account savedAccount = accountRepository.save(account);
+
         return AccountMapper.mapAccountToAccountDto(savedAccount);
     }
 
     @Override
     public AccountDto withdraw(Long accountId, Double balance) {
         Account account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
+                .orElseThrow(() -> new AccountNotFoundException(
+                        "Account not found with id: " + accountId));
 
         if (account.getBalance() < balance) {
-            throw new RuntimeException("Insufficient balance for withdrawal.");
+            throw new InSufficientBalanceException("Insufficient balance for withdrawal.");
         }
 
         double total = account.getBalance() - balance;
         account.setBalance(total);
+
         Account savedAccount = accountRepository.save(account);
+
         return AccountMapper.mapAccountToAccountDto(savedAccount);
     }
 
     @Override
     public List<AccountDto> getAllAccounts() {
         List<Account> account = accountRepository.findAll();
-        return account.stream().map(AccountMapper::mapAccountToAccountDto).toList();
 
+        return account.stream()
+                .map(AccountMapper::mapAccountToAccountDto)
+                .toList();
     }
 
     @Override
     public void deleteAccount(Long accountId) {
-        Account account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
+        accountRepository.findById(accountId)
+                .orElseThrow(() -> new AccountNotFoundException(
+                        "Account not found with id: " + accountId));
+
         accountRepository.deleteById(accountId);
     }
-
 }
